d0486e8ee86756c96da11caa5ce656d682e4db7f
Cam <camhack67@gmail.com>
2023-07-25 19:26:30 -0400

    fixed bug where the combinedpassword var kept stacking onto the array everytime you generated a password

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-
+// variables defining what characters can be used in your password
 let lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let symbols = ['!', '?', '@', '#', '$', '%', '^', '&', '*', '/', '-', '_', '=', '+'];
-let combinedPassword = [];
-let finalPass = "";
 
+
+// this block generates the password for you
 function generatePassword(){
+// resets password each time you hit Generate Password so you dont have to refresh :)
+  let finalPass = "";
+  let combinedPassword = [];
+
   if (confirm('Do you want password to contain lowercase letters?')){
     combinedPassword = [...combinedPassword, ...lowerCaseLetters]
   }
@@ -29,7 +32,7 @@ function generatePassword(){
 
   let passwordLength = prompt('Enter a password length between 8 and 128 characters');
 
-  if (passwordLength > 8 && passwordLength < 128) {
+  if (passwordLength >= 8 && passwordLength <= 128) {
     for (let i = 0; i < passwordLength; i++) {
       let genCharacterIndex = Math.floor(Math.random()*combinedPassword.length);
       finalPass += combinedPassword[genCharacterIndex]
@@ -41,8 +44,12 @@ function generatePassword(){
     passwordLength = confirm('😡 Invalid password length please TRY AGAIN! 😡')
   }
 
+  combinedPassword = []
+
   return finalPass
 }
+
+// Write password to the #password input
 
 function writePassword() {
   var password = generatePassword();
