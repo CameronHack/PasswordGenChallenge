e4e2c329f7a4f68af1fc9bedfecbbb6a80f50e51
Cam <camhack67@gmail.com>
2023-07-26 09:58:42 -0400

    accessibility comments

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// variables defining what characters can be used in your password
+// variables defining what characters can be used in password generation
 let lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let symbols = ['!', '?', '@', '#', '$', '%', '^', '&', '*', '/', '-', '_', '=', '+'];
 
-
-// this block generates the password for you
-function generatePassword(){
+// this block does the math to generate the password for you
+function generatePassword() {
+  // final password value
   let finalPass = "";
+  // array of selected characters from the options below
   let combinedPassword = [];
 
-  if (confirm('Do you want password to contain lowercase letters?')){
+  // ask user what characters should be present in their generated password
+  if (confirm('Do you want password to contain lowercase letters?')) {
     combinedPassword = [...combinedPassword, ...lowerCaseLetters]
   }
-  if (confirm('Do you want password to contain uppercase letters?')){
+  if (confirm('Do you want password to contain uppercase letters?')) {
     combinedPassword = [...combinedPassword, ...upperCaseLetters]
   }
-  if (confirm('Do you want password to contain numbers?')){
+  if (confirm('Do you want password to contain numbers?')) {
     combinedPassword = [...combinedPassword, ...numbers]
   }
-  if (confirm('Do you want password to contain symbols?')){
+  if (confirm('Do you want password to contain symbols?')) {
     combinedPassword = [...combinedPassword, ...symbols]
   }
-  
-  console.log("combinedPassword array: " + combinedPassword)
-  console.log("combinedPassword length: " + combinedPassword.length)
+
+  // asks the user to type in desired password length
   let passwordLength = prompt('Enter a password length between 8 and 128 characters');
-  
+
+  // if password length is greater than or equal to 8, less than or equal to 128, and your selected character array is greater than 0 your password will be generated
   if (passwordLength >= 8 && passwordLength <= 128 && combinedPassword.length > 0) {
+    // generates a password based off the acceptable length you chose and the options of characters you decided in the above prompts
     for (let i = 0; i < passwordLength; i++) {
-      let genCharacterIndex = Math.floor(Math.random()*combinedPassword.length);
+      let genCharacterIndex = Math.floor(Math.random() * combinedPassword.length);
       finalPass += combinedPassword[genCharacterIndex]
-      console.log("combinedPassword array: " + combinedPassword)
-      console.log("combinedPassword length: " + combinedPassword.length)
-      console.log('Your final password: ' + finalPass)
-      console.log(genCharacterIndex)
+      // useful logs
+      // console.log("combinedPassword array: " + combinedPassword)
+      // console.log("combinedPassword length: " + combinedPassword.length)
+      // console.log(genCharacterIndex)
     }
   } else {
+    // if your password didnt fall into the acceptable selections then you will be prompted to try again
     confirm('😡 Invalid password length and/or no characters selected please TRY AGAIN! 😡')
   }
 
-  console.log('Your final password: ' + finalPass)
-  console.log('successful return')
+  // returns the final generated password to generatePassword function to be displayed
+  // console.log('Your final password: ' + finalPass)
+  // console.log('successful return')
   return finalPass
 
 }
